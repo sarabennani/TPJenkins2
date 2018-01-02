eef58835598d688c6fe1d07c7f4e1d35a791f4c3
Sara <sara.bennani26@gmail.com>
2018-01-02 09:09:37 +0000

    Merge newmehdi into swpsaraa
    Conflicts:
    	build/web/WEB-INF/classes/com/swp/servlets/dept/GestionEmploi.class
    	build/web/WEB-INF/viewsys/AjouterUtilisateur.jsp
    	nbproject/private/private.properties

---
+++ src/main/java/Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Maven 3.5.2'
+        jdk 'jdk1.8.0_151'
+    }
+    stages {
+        
+        stage ('Build') {
+            steps {
+            bat 'mvn install'
+            }
+            post {
+                success {
+                    junit 'target/surefire-reports/**/*.xml' 
+                }
+            }
+        }
+    }
+}
